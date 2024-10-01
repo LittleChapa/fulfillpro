0dd121d52e5eba0da5d4f225db7bc4b20ca3f70d
LittleChapa <daaavid943@gmail.com>
2024-10-01 19:31:57 +0300

    docker init

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 require('dotenv').config();
 const express = require('express');
-const axios = require('axios');
 const path = require('path');
 const router = require('./routes');
 const cors = require('cors');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors('*'));
+app.use(
+  cors({
+    origin: process.env.CLIENTURL,
+  })
+);
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use('/api', router);
